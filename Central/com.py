6651c05f7260ffc57af11e6a64ac2b2b27bbd6cb
renan601 <welzrenan@gmail.com>
2022-12-16 23:07:16 -0300

    Added new functions to Central Class and comunication module.

--- Central/com.py
+++ Central/com.py
@@ -1,18 +1,24 @@
 import socket
 import json
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+def stablish_communication():
+    HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+    PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            sensor_status = json.loads(data)
-            if not data:
-                break
-            conn.sendall(data)
+    room_name = conn.recv(1024)
+    return conn, addr, room_name
+
+def send_data_to_client(data, conn, addr):
+    import ipdb; ipdb.set_trace()
+    print(f"Connected by {addr}")
+    sensor_status = (bytes(json.dumps(data), encoding='utf-8'))
+    conn.sendall(sensor_status)
+    #client_data = conn.recv(1024)
+
+def receive_data_from_client():
+    pass
--- Central/main.py
+++ Central/main.py
@@ -1,12 +1,26 @@
+from com import stablish_communication, send_data_to_client, receive_data_from_client
+from display import Display
+
 class Central:
     def __init__(self):
         self.lampada1 = False
         self.lampada2 = False
         self.arCondicionado = False
         self.projetor = False
-        
+
+        self.client_data = []
+
+        self.connect_to_clients()
+
         self.menu()
     
+    def connect_to_clients(self):
+        dist_count = int(input('Quantos servidores distribuidos serão conectados? \n'))
+
+        for i in range(dist_count):
+            com_socket, client_addr, client_room = stablish_communication()
+            self.client_data.append((client_room, com_socket, client_addr))
+
     def menu(self):
         menu = {
             1: 'Acender Lâmpada 01',
@@ -15,35 +29,36 @@ class Central:
             4: 'Ligar Ar-Condicionado (1º Andar)',
             5: 'Ligar Sistema de Alarme',
             6: 'Ligar Sistema de Alerta de Incêndio',
-            7: 'Sair'
+            7: 'Voltar a Tela de Monitoramento'
         }
 
-        for key in menu.keys():
-            print (key, '--', menu[key])
-        
-        option = ''
-        
-        try:
-            option = int(input('Escolha uma das opções acima \n'))
-        except:
-            print('Opção inválida. Digite um número de 1 a 7')
+        while True:
+            for key in menu.keys():
+                print (key, '--', menu[key])
+            
+            option = ''
+            
+            try:
+                option = int(input('Escolha uma das opções acima \n'))
+            except:
+                print('Opção inválida. Digite um número de 1 a 7')
 
-        if option == 1:
-            self.modify_device_status(option)
-        elif option == 2:
-            self.modify_device_status(option)
-        elif option == 3:
-            self.modify_device_status(option)
-        elif option == 4:
-            self.modify_device_status(option)
-        elif option == 5:
-            self.modify_system_alarm_status()
-        elif option == 6:
-            self.modify_fire_alarm_status()
-        elif option == 7:
-            exit()
-        else:
-            print('Opção inválida. Digite um número de 1 a 7')
+            if option == 1:
+                self.modify_device_status(option)
+            elif option == 2:
+                self.modify_device_status(option)
+            elif option == 3:
+                self.modify_device_status(option)
+            elif option == 4:
+                self.modify_device_status(option)
+            elif option == 5:
+                self.modify_system_alarm_status()
+            elif option == 6:
+                self.modify_fire_alarm_status()
+            elif option == 7:
+                exit()
+            else:
+                print('Opção inválida. Digite um número de 1 a 7')
 
     def modify_device_status(self, option):
         if option == 1:
@@ -66,6 +81,8 @@ class Central:
                 "type": "ar-condicionado",
                 "tag": "Ar-Condicionado (1º Andar)"
             }
+
+        send_data_to_client(demand_content, self.client_data[0][1], self.client_data[0][2])
 
     def modify_system_alarm_status(self):
         pass
